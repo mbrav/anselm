f190f2324de93730763464511777a84ea5cd3bfb
mbrav <mbrav@protonmail.com>
2024-06-07 13:25:54 +0300

    Update sql scripts

--- sql/volume_date_range.sql
+++ sql/volume_date_range.sql
@@ -2,7 +2,7 @@ SELECT
     toDate(begin) AS day,
     formatReadableQuantity(sum(volume)) AS total_volume
 FROM
-    md_moex.candles
+    md_moex.trades
 WHERE
     begin >= '2024-01-01' AND begin < '2024-06-01'
 GROUP BY
