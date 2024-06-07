63122bf10517d9549bc2fc1d79de2602cb072013
mbrav <mbrav@protonmail.com>
2024-06-07 13:10:48 +0300

    Remove legacy

--- anselm_scribe/src/db.rs
+++ anselm_scribe/src/db.rs
@@ -37,33 +37,9 @@ impl ClickhouseDatabase {
             .await?;
 
         // Init tables
-        self.init_security().await?;
         self.init_board().await?;
-        self.init_candles().await?;
         self.init_trades().await?;
 
-        Ok(())
-    }
-
-    /// Init security table
-    pub async fn init_security(&self) -> Result<()> {
-        self.client
-            .query(
-                "
-                CREATE TABLE IF NOT EXISTS ?.securities(
-                    secid       String,
-                    boardid     String,
-                    shortname   String,
-                    status      String,
-                    marketcode  String,
-                )
-                ENGINE = MergeTree
-                PRIMARY KEY secid
-                ",
-            )
-            .bind(sql::Identifier(self.db.as_str()))
-            .execute()
-            .await?;
         Ok(())
     }
 
@@ -116,84 +92,6 @@ impl ClickhouseDatabase {
                 ENGINE = MergeTree
                 PARTITION BY toYYYYMM(tradetime)
                 ORDER BY (engine, market, secid, boardid, tradetime, tradeid);
-                ",
-            )
-            .bind(sql::Identifier(self.db.as_str()))
-            .execute()
-            .await?;
-        Ok(())
-    }
-
-    /// # Table Optimizations
-    /// In order to provide an efficient way for storing and querying data, a few important
-    /// decisions and optimizations were made when defining the table schema.
-    ///
-    /// ## Table Schema
-    /// The `candles` table stores the OHLCV (Open, High, Low, Close, Volume) data for
-    /// securities over different time frames. Each record in this table represents a single
-    /// candle for a specific security and time frame.
-    ///
-    /// The columns in the table are as follows:
-    /// - `secid`: The unique identifier for the security. It is stored as a
-    ///   `LowCardinality(String)` for efficient storage and querying of categorical data.
-    /// - `timeframe`: The time frame for the candle (e.g., 1 minute, 5 minutes, etc.). It is
-    ///   stored as an `Int16` and uses the `Codec(Delta, Default)` codec for efficient storage.
-    /// - `open`: The opening price of the security for the candle's time frame.
-    /// - `close`: The closing price of the security for the candle's time frame.
-    /// - `high`: The highest price of the security for the candle's time frame.
-    /// - `low`: The lowest price of the security for the candle's time frame.
-    /// - `value`: The total value traded during the candle's time frame.
-    /// - `volume`: The total volume traded during the candle's time frame.
-    /// - `begin`: The start time of the candle.
-    /// - `end`: The end time of the candle.
-    ///
-    /// ## Storing Only Deltas
-    /// For columns where values typically change incrementally, the `Codec(Delta, Default)`
-    /// keyword is used. This codec helps in reducing the storage space required by storing only
-    /// the differences (deltas) between consecutive values, rather than the full values. This
-    /// is particularly useful for columns like `open`, `close`, `high`, `low`, `value`, and
-    /// `volume`, where the changes between rows are often small.
-    ///
-    /// For example:
-    ///
-    /// ```sql
-    /// open       Nullable(Float64) Codec(Delta, Default),
-    /// close      Nullable(Float64) Codec(Delta, Default),
-    /// ```
-    ///
-    /// By using the `Codec(Delta, Default)`, the storage engine can store the difference
-    /// between consecutive values, which can lead to significant space savings.
-    ///
-    /// ## ClickHouse Engine and Order
-    /// The table uses the `MergeTree` engine, which is optimized for analytical queries. The
-    /// `ORDER BY` clause specifies that the data should be ordered by `secid` and `begin`,
-    /// allowing efficient range queries based on security ID and time.
-    ///
-    /// ## Resources
-    ///  - [Optimizing ClickHouse with Schemas and Codecs](https://clickhouse.com/blog/optimize-clickhouse-codecs-compression-schema)
-    ///  - [Using ClickHouse for financial market data - Christoph Wurm (ClickHouse)](https://youtu.be/Ojv6LPXKy2U?si=Je8BkFA8nOTczLZn)
-    ///  - [New Tips and Tricks that Every ClickHouse Developer Should Know | ClickHouse Webinar](https://youtu.be/BLt246SijGU?si=4yBgOVfvfjs-34Qc)
-    pub async fn init_candles(&self) -> Result<()> {
-        self.client
-            .query(
-                "
-                CREATE TABLE IF NOT EXISTS ?.candles(
-                    secid      LowCardinality(String) Codec(ZSTD(1)),
-                    boardid    LowCardinality(String) Codec(ZSTD(1)),
-                    shortname  LowCardinality(String) Codec(ZSTD(1)),
-                    timeframe  Int16 Codec(Delta, Default),
-                    open       Nullable(Float64) Codec(Delta, Default),
-                    close      Nullable(Float64) Codec(Delta, Default),
-                    high       Nullable(Float64) Codec(Delta, Default),
-                    low        Nullable(Float64) Codec(Delta, Default),
-                    value      Nullable(Float64) Codec(Delta, Default),
-                    volume     Nullable(Float64) Codec(Delta, Default),
-                    begin      DateTime Codec(DoubleDelta, ZSTD(1)),
-                    end        DateTime Codec(DoubleDelta, ZSTD(1)),
-                )
-                ENGINE = MergeTree
-                PARTITION BY toYYYYMM(begin)
-                ORDER BY (secid, begin, end)
                 ",
             )
             .bind(sql::Identifier(self.db.as_str()))
